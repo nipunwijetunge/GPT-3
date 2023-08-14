6cf24283c1dc82f7a205d1d5488420ed513f2da9
Nipun Wijetunge <57147260+nipunwijetunge@users.noreply.github.com>
2023-08-14 09:51:01 +0530

    what is GPT-3

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -3,7 +3,17 @@ import React from "react";
 import "./features.css";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
