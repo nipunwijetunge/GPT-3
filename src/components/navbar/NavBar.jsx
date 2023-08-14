4f7549e2656f54316f19d564a7b78e603da15cd0
Nipun Wijetunge <57147260+nipunwijetunge@users.noreply.github.com>
2023-08-14 10:24:59 +0530

    case studies

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -13,10 +13,10 @@ const Menu = () => (
       <a href="#wgpt3">what is GPT3?</a>
     </p>
     <p>
-      <a href="#possibility">OpenAI</a>
+      <a href="#features">Case Studies</a>
     </p>
     <p>
-      <a href="#features">Case Studies</a>
+      <a href="#possibility">OpenAI</a>
     </p>
     <p>
       <a href="#blog">Library</a>
