bc87647ba9a82f334fe845eb93b71d8bee509884
Nipun Wijetunge <57147260+nipunwijetunge@users.noreply.github.com>
2023-08-14 14:28:31 +0530

    blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ image, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={image} alt={title} />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
